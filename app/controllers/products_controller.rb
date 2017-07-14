cef8edfb92e1759184acfdf05532d42b937b440b
mavyrick <jrsorokin@gmail.com>
2017-07-13 19:27:34 -0700

    Day 2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,21 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.order(created_at: :desc).page(params[:page]).per(10)
+    @products = Product.all
     @categories = Category.all
+    respond_to do |format|
+      format.html { render }
+      format.json { render json: @products }
+    end
   end
 
   def show
     @product = Product.find params[:id]
+    @categories = Category.all
+    respond_to do |format|
+      format.html { render }
+      format.json { render json: @product }
+    end
   end
 
 end
